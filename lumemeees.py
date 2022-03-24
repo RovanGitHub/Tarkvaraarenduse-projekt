c620baf8a59eae97f07a1e6a7daa09909a514349
RovanGitHub <102147565+RovanGitHub@users.noreply.github.com>
2022-03-24 14:05:38 +0200

    Add files via upload

---
+++ lumemeees.py
@@ -0,0 +1,33 @@
+import pygame  # Importeerib pygame
+
+pygame.init()  # Käivitab pygame mooduli
+
+screen = pygame.display.set_mode([300, 500])  # Teeb 300x500 ekraani
+
+pygame.display.set_caption("Lumemees - Rovan Kütt")  # Paneb akna nimeks Rovan Kütt
+
+pygame.draw.circle(screen, [255, 255, 255], [150, 85], 30, 0)  # Teeb valge lumememme pea
+pygame.draw.circle(screen, [0, 0, 0], [138, 82], 5, 0)  # Teeb musta lumememme vasaku silma
+pygame.draw.circle(screen, [0, 0, 0], [162, 82], 5, 0)  # Teeb musta lumememme parema silma
+pygame.draw.polygon(screen, [255, 0, 0], [[145, 90], [155, 90], [150, 105]], 0)  # Teeb punase lumememme nina
+pygame.draw.circle(screen, [255, 255, 255], [150,150], 40, 0)  # Teeb valge lumememme keskmise palli
+pygame.draw.circle(screen, [255, 255, 255], [150, 235], 50, 0)  # Teeb valge lumememme alumise palli
+pygame.draw.line(screen, [165, 42, 42], [120,120], [50,110], 4)
+pygame.draw.line(screen, [165, 42, 42], [180,120], [240,120], 4)
+pygame.draw.line(screen, [165, 42, 42], [225,78], [241,117], 4)
+pygame.draw.line(screen, [165, 42, 42], [260,130], [266,80], 4)
+
+pygame.display.flip()  # Värskendab ekraani
+
+for i in range(9999):
+    x,y = pygame.mouse.get_pos()
+    print(x, y)
+
+# Internetist võetud, et akent kinni panna.
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        if running == False:
+            pygame.quit()
