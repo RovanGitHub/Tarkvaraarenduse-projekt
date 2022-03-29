5c2ba2a74158a92b28371a329dba87df2b869366
mrohtla <102147567+mrohtla@users.noreply.github.com>
2022-03-29 13:04:36 +0300

    Update foor2.py

--- foor2.py
+++ foor2.py
@@ -8,13 +8,13 @@ pygame.draw.circle(screen, [255, 0, 0], [150,60], 40, 0) # teeb punase täidetud
 pygame.draw.circle(screen, [255, 255, 0], [150,145], 40, 0)# teeb kollase täidetud ringi x = 139, y=150, raaduius=40 joone paksus=0 et täidaks.
 pygame.draw.circle(screen, [102, 255, 51], [150,230], 40, 0)# teeb rohelise täidetud ringi x = 139, y=235, raaduius=40 joone paksus=0 et täidaks.
 
-pygame.draw.rect(screen, [128, 128, 128], [145, 280, 10, 150], 0)
-pygame.draw.polygon(screen, [0, 0, 255], [[100,430],[200,430],[215,460],[85,460],[100,430]], 0)
-pygame.draw.polygon(screen, [255, 255, 255], [[65,495],[235,495],[255,530],[50,530],[65,495]], 0)
-pygame.draw.polygon(screen, [128, 128, 128], [[100,430],[200,430],[255,530],[50,530],[65,495]], 2)
+pygame.draw.rect(screen, [128, 128, 128], [145, 280, 10, 150], 0)#teeb halli ristküliku x=145, y=280, w=10, h=150
+pygame.draw.polygon(screen, [0, 0, 255], [[100,430],[200,430],[215,460],[85,460],[100,430]], 0)#teeb sisnise täidetud hulknurga mille tippude koordinaadid (100,430), (200,430), (215,460), (85, 460), (100,430)
+pygame.draw.polygon(screen, [255, 255, 255], [[65,495],[235,495],[255,530],[50,530],[65,495]], 0)#teeb valge täidetud hulknurga tipu koordinaatidega (65,495),(235,495),(255,530),(50,530),(65,495)
+pygame.draw.polygon(screen, [128, 128, 128], [[100,430],[200,430],[255,530],[50,530],[65,495]], 2)#teeb halli hulknurga tipu koordinaatidega (100,430),(200,430),(255,530),(50,530),(65,495)
 
 
-#/_\
+
 pygame.display.flip()# värskendab ekraani
 
 
