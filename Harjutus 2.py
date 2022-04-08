761fd69fcdbd713de6b9560e85c9801114792409
RovanGitHub <102147565+RovanGitHub@users.noreply.github.com>
2022-04-08 10:26:17 +0300

    Update Harjutus 2.py

--- Harjutus 2.py
+++ Harjutus 2.py
@@ -1,30 +1,37 @@
 import pygame
 pygame.init()
 
+# Github: https://github.com/RovanGitHub/Tarkvaraarenduse-projekt/blob/e5bd40bc4b9188fb8f511707c2b0c0c6bfe5cb89/Harjutus%202.py
+
 #ekraani seaded
-screen=pygame.display.set_mode([640,480])
-pygame.display.set_caption("Harjutus 2")
-screen.fill([204, 255, 204])
+screen=pygame.display.set_mode([640,480]) # teeb akna suurusega 640x480
+pygame.display.set_caption("Harjutus 2") # paneb akna nimeks Harjutus 2
 
-bg = pygame.image.load("bg_shop.jpg")
-screen.blit(bg,[0,0])
+# Pood
+bg = pygame.image.load("bg_shop.jpg") # seab akna taustaks poe pildi
+screen.blit(bg,[0,0]) # toob pildi välja ja paigutab
 
-seller = pygame.image.load("seller.jpg")
-seller = pygame.transform.scale(seller, [252, 303])
-screen.blit(seller,[103,161])
+# Mehike
+seller = pygame.image.load("seller.jpg") # toob sisse mehikese pildi
+seller = pygame.transform.scale(seller, [252, 303]) # muudab pildi suurust
+screen.blit(seller,[103,161]) # toob mehikese välja ja muuda asukohta
 
-mull = pygame.image.load("chat.jpg")
-mull = pygame.transform.scale(mull, [258, 203])
-screen.blit(mull,[244,65])
+# Jutumull
+mull = pygame.image.load("chat.jpg") # toob sisse jutumulli
+mull = pygame.transform.scale(mull, [258, 203]) # muudab jutumulli suurust
+screen.blit(mull,[244,65]) # toob jutumulli välja ja muudab asukohta
 
 #lisame teksti
-font = pygame.font.SysFont('arial', 22, bold=True)
-text = font.render("Tere, olen Rovan Kütt", True, [255,255,255])
-screen.blit(text, [280,135])
-
-pygame.display.flip()
+font = pygame.font.SysFont('arial', 22, bold=True) # seadistab fondi arial, suurus 22 ja teeb boldiks
+text = font.render("Tere, olen Rovan Kütt", True, [255,255,255]) # seab teksti ja teksti valge värvina
+screen.blit(text, [280,135]) # toob teksti välja ja muudab asukohta
 
 
+pygame.display.flip() # värskendab ekraani
+
+
+
+# selle jaoks et aken ära ei kaoks ja kinni saaks panna, internetist võetud.
 running = True
 while running:
     ev = pygame.event.get()
