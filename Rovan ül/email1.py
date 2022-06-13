1d71ed5c3ae4ad0dccf82c5d96706eef1fc24db3
rovan.kutt <rovan.kutt@vikk.ee>
2022-06-13 15:20:34 +0300

    Yoyoyo

--- Rovan ül/email1.py
+++ Rovan ül/email1.py
@@ -3,7 +3,10 @@ import smtplib # impordib smtplib
 sender_email = "vikk.opilane@outlook.com" # kes saadab meili
 rec_email = "rovan.kutt@vikk.ee" # kellele saadab meili
 password = input(str("Please enter your password: ")) # küsib meili parooli
-message = "Hey, this was sent using python" # seab teksti mis saadab
+
+SUBJECT = "Hello!" # Seab teema
+TEXT = "This message was sent with Python's smtplib." # Seab teksti sisu
+message = 'Subject: {}\n\n{}'.format(SUBJECT, TEXT) # Koostab meili
 
 server = smtplib.SMTP('smtp-mail.outlook.com', 587) # sisestab emaili
 server.starttls() # alustab starttlsi
